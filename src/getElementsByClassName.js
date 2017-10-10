ce4e55c59e1f9d7e6b4f6bef9ddf1a048d667f8c
Hack Reactor Student <student@hackreactor.com>
2017-10-10 15:02:36 -0700

    Completed functional getElementsByClassName function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,32 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // Array to store results
+  // Func with node as input
+  //   Check if node has className
+  //     If so push to array        
+  //   Check if node has a length
+  //     If so call func
+  // Call func on document.body
+
+
+  let results = [];
+
+  let searchNodes = function(node) {
+    if (node.classList && node.classList.contains(className)) {
+      results.push(node);
+    }
+    let children = node.childNodes;
+
+    if (children.length) {
+      for (let i = 0; i < children.length; i++) {
+        searchNodes(children[i]);
+      }
+    }
+  };
+
+  searchNodes(document.body);
+  return results;
+
 };
